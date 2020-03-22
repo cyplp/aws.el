e8abc1ec186f2adb290dfa7b77c24b472586fecf
Cyprien Le Pannérer <cyplp@free.fr>
2020-03-22 09:49:47 +0100

    some lint corrections

--- aws.el
+++ aws.el
@@ -1,21 +1,26 @@
-;;; -*- lexical-binding: t; -*-
+;;; package --- aws.el
+;;; Commentary: some draft
 
+;;; Code:
 
 (require 'async)
 (require 'parse-time)
 (require 'transient)
 (require 'term)
+
 (with-no-warnings
   (require 'cl))
 
 (setq lexical-binding t)
 
 (defun cyplp-aws-read-file (f)
+  ;; Helper for read file
   (with-temp-buffer
     (insert-file-contents f)
     (buffer-string)))
 
 (defun not-nil (value)
+  "Replace nil by empty string if value is nil."
   (if value
       value
       ""))
